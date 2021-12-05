4c4ba308d6d0b8843141f8739725a929a1423852
Yassine najem <yasen2441@gmail.com>
2021-12-05 20:01:09 +0100

    small fix

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -11,7 +11,7 @@ export const fetchApi = async (url) => {
   const config = {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': 'dbdc1e7380mshf23d625a1e44e54p149710jsneba1ec309d1e'
+      'x-rapidapi-key': process.env.API_KEY,
     }
   };
   const { data } = await axios.get((url), config);
